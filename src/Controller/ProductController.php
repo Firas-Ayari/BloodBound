832fd9838f89f3a37fc5f09d142c67b913a9766c
Firas Ayari <spy.firas16@gmail.com>
2023-03-05 18:42:53 +0100

    Users & MarketPlace Merge fix

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -15,6 +15,11 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\String\Slugger\SluggerInterface;
+
+
 
 #[Route('/product')]
 class ProductController extends AbstractController
@@ -51,15 +56,47 @@ class ProductController extends AbstractController
     }
 
     #[Route('/new', name: 'app_product_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, ProductRepository $productRepository): Response
+    public function new(Request $request, ProductRepository $productRepository, SluggerInterface $slugger): Response
     {
         $product = new Product();
         $form = $this->createForm(ProductType::class, $product);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            
+
+            //upload img
+            $image = $form->get('image')->getData();
+
+            // this condition is needed because the 'image' field is not required
+            // so the PDF file must be processed only when a file is uploaded
+            if ($image) {
+                $originalFilename = pathinfo($image->getClientOriginalName(), PATHINFO_FILENAME);
+                // this is needed to safely include the file name as part of the URL
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename.'-'.uniqid().'.'.$image->guessExtension();
+
+                // Move the file to the directory where images are stored
+                try {
+                    $image->move(
+                        $this->getParameter('img_directory'),
+                        $newFilename
+                    );
+
+                    
+                } catch (FileException $e) {
+                    // ... handle exception if something happens during file upload
+                }
+
+                // updates the 'imageename' property to store img name
+                // instead of its contents
+                $product->setImage($newFilename);
+                
+            }
+
             $this->em->persist($product);
             $this->em->flush();
+
             return $this->redirectToRoute('app_product_index_admin', [], Response::HTTP_SEE_OTHER);
         }
 
@@ -133,13 +170,42 @@ class ProductController extends AbstractController
     }
 
     #[Route('/{id}/edit', name: 'app_product_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, Product $product, ProductRepository $productRepository, $id): Response
+    public function edit(Request $request, Product $product, ProductRepository $productRepository, $id , SluggerInterface $slugger): Response
     {
         $product = $this->productRepository->find($id);
         $form = $this->createForm(ProductType::class, $product);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+                        //upload img
+                        $image = $form->get('image')->getData();
+
+                        // this condition is needed because the 'image' field is not required
+                        // so the PDF file must be processed only when a file is uploaded
+                        if ($image) {
+                            $originalFilename = pathinfo($image->getClientOriginalName(), PATHINFO_FILENAME);
+                            // this is needed to safely include the file name as part of the URL
+                            $safeFilename = $slugger->slug($originalFilename);
+                            $newFilename = $safeFilename.'-'.uniqid().'.'.$image->guessExtension();
+            
+                            // Move the file to the directory where images are stored
+                            try {
+                                $image->move(
+                                    $this->getParameter('img_directory'),
+                                    $newFilename
+                                );
+            
+                                
+                            } catch (FileException $e) {
+                                // ... handle exception if something happens during file upload
+                            }
+            
+                            // updates the 'imageename' property to store img name
+                            // instead of its contents
+                            $product->setImage($newFilename);
+                            
+                        }
+            
             $this->em->flush();
 
             return $this->redirectToRoute('app_product_index_admin', [], Response::HTTP_SEE_OTHER);
